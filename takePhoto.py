bbc599a249682c459cc8f8f487d189d93d0a929c
Stuart Wilson <stuart@stuartwilsondev.com>
2014-07-25 17:24:03 +0100

    Swap direct console prints out for a logging function. Add main() method add flag for log to console (-l)

--- takePhoto.py
+++ takePhoto.py
@@ -2,8 +2,46 @@
 
 import os
 import time
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import logging
 from datetime import datetime
+import argparse
+
+log_to_screen = False
+
+#Set global logger
+logging.basicConfig(
+  format='%(asctime)s:%(filename)s:%(levelname)s:__%(message)s',
+  filename='photo.log',
+  level=logging.INFO
+)
+
+def log_message(message,level='info'):
+
+  if log_to_screen:
+    print "LOG:: %s" % message
+
+  if level == 'debug':
+    logging.debug(message)
+
+  elif level == 'info':
+    logging.info(message)
+
+  elif level == 'warning':
+    logging.warning(message)
+
+  elif level == 'error':
+    logging.error(message)
+
+  elif level == 'critical':
+    logging.critical(message)
+
+  else:
+    logging.info(message)
+
+
+
+
 
 class UniqueDirectory(object):
 
@@ -29,9 +67,9 @@ class UniqueDirectory(object):
     #create it if it does not exist
     if not os.path.isdir(directory_name):
       os.mkdir(directory_name)
-      print "Created Directory: %s" % directory_name
+      log_message("Created Directory: %s" % directory_name)
     else:
-      print "Directory: %s already exists" % directory_name
+      log_message("Directory: %s already exists" % directory_name)
 
     return directory_name
 
@@ -87,13 +125,32 @@ class TimeLapseImage(object):
         self.get_file_name()
       )
 
-    print "Command to run: %s" % command
+    log_message("Command to run: %s" % command)
 
     return command
 
 
-Directory = UniqueDirectory()
-Img = TimeLapseImage(800,600,Directory)
+def main():
 
-#Run the command
-os.system(Img.get_command())
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-l', action='store_true', default=False,
+      help='Log to the console as well as file')
+
+    args = parser.parse_args()
+
+    global log_to_screen
+    log_to_screen = args.l
+
+    log_message('Started')
+
+    Directory = UniqueDirectory()
+    Img = TimeLapseImage(800,600,Directory)
+
+    #Run the command
+    os.system(Img.get_command())
+
+    log_message('Finished')
+
+if __name__ == '__main__':
+    main()
